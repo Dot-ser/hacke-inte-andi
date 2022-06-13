c79c92406ecb298935364d7bc1e5a897f287599e
Dot-ser <105029178+Dot-ser@users.noreply.github.com>
2022-06-13 10:51:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const hex = "DoT-sEr!dOT-SeR".split("");
 setInterval(() => {
     ctx.fillStyle = "rgba(0, 0, 0, .05)";
     ctx.fillRect(0, 0, w, h);
-    ctx.fillStyle = "green";
+    ctx.fillStyle = "red";
     p.map((v, i) => {
         ctx.fillText(random(hex), i * 10, v);
         p[i] = v >= h || v > 50 + 10000 * Math.random() ? 0 : v + 10;
